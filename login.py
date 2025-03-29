e601657373b39e750ac9f195032fa30a12f5ec9a
Hideous <41999240+JJun2319@users.noreply.github.com>
2025-03-29 23:16:38 +0900

    Update login.py

--- login.py
+++ login.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from time import sleep
 driver = webdriver.Chrome("C:\\Users\\조영준\\Desktop\\a\\chromedriver.exe")
 def login():
-    driver.find_element_by_name('userid').send_keys('조영준')
-    driver.find_element_by_name('pwd').send_keys("Jun1796!")
+    driver.find_element_by_name('userid').send_keys('YOUR ID')
+    driver.find_element_by_name('pwd').send_keys("put ur own pwd")
     sleep(0.5)
     driver.find_element_by_xpath('//*[@id="blog"]/div/div[1]/form/table/tbody/tr[1]/td[3]/div').click()
 
